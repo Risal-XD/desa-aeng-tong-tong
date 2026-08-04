bc855555f56823a1ff74583eb37ea196ca84d99e
Admin Desa <admin@aengtongtong.desa.id>
2026-08-04 21:35:24 +0700

    Fine-tune foto hero sejarah: background-size 90% (zoom out sedikit, optimal balance)

--- resources/views/frontend/about/sejarah.blade.php
+++ resources/views/frontend/about/sejarah.blade.php
@@ -9,7 +9,7 @@
         subtitle="Perjalanan panjang Desa Aeng Tong-Tong hingga menjadi desa wisata keris yang dikenal luas."
         :image="$heroImage"
         imagePosition="right"
-        backgroundSize="contain"
+        backgroundSize="90%"
     />
 
     <section class="mx-auto max-w-6xl px-4 py-16 sm:px-6">
