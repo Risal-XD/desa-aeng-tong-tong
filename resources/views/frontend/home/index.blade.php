ad2836a907c8e18c7541dcd1f872467473847b5d
Admin Desa <admin@aengtongtong.desa.id>
2026-08-07 19:16:35 +0700

    Hero beranda: perkuat margin negatif -mt-16 pada kolom kanan agar frame foto naik sejajar ke atas

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -29,7 +29,7 @@
                 </div>
             </div>
 
-            <div class="hidden lg:block -mt-4">
+            <div class="hidden lg:block -mt-16">
                 <div x-data="fadeSlider(@js($heroPhotos->map(fn ($photo) => [
                     'title' => $photo->title,
                     'image' => $photo->image ? asset('storage/'.$photo->image) : null,
